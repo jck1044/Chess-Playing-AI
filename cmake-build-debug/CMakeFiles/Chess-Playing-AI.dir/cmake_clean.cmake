a40632889d0874c8de65c58f64919aae363f2063
jck1044 <jck1044@wildcats.unh.edu>
2023-06-07 22:27:55 -0400

    Initial commit: playable game, no ending of game

---
+++ cmake-build-debug/CMakeFiles/Chess-Playing-AI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess-Playing-AI.dir/main.cpp.o"
+  "CMakeFiles/Chess-Playing-AI.dir/main.cpp.o.d"
+  "Chess-Playing-AI"
+  "Chess-Playing-AI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess-Playing-AI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
