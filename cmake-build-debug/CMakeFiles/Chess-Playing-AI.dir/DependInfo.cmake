33e6d0234ffcb0dde0e176638cef41135d715338
jaredking <jared.king1351@gmail.com>
2025-03-03 17:46:26 -0500

    can debug

---
+++ cmake-build-debug/CMakeFiles/Chess-Playing-AI.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jaredking/Desktop/Chess-Playing-AI/chess.cpp" "CMakeFiles/Chess-Playing-AI.dir/chess.cpp.o" "gcc" "CMakeFiles/Chess-Playing-AI.dir/chess.cpp.o.d"
+  "/Users/jaredking/Desktop/Chess-Playing-AI/gameplay.cpp" "CMakeFiles/Chess-Playing-AI.dir/gameplay.cpp.o" "gcc" "CMakeFiles/Chess-Playing-AI.dir/gameplay.cpp.o.d"
+  "/Users/jaredking/Desktop/Chess-Playing-AI/gui.cpp" "CMakeFiles/Chess-Playing-AI.dir/gui.cpp.o" "gcc" "CMakeFiles/Chess-Playing-AI.dir/gui.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
